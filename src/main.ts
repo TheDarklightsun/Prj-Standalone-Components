4459496862d3911e37c2f1314d21016c9aeba77b
TheDarklightsun <bublik.kirill2012@gmail.com>
2023-11-20 02:40:47 +0200

    add-services-routing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import {bootstrapApplication} from "@angular/platform-browser";
+import {importProvidersFrom} from "@angular/core";
 
 import {AppComponent} from "./app/app.component";
+import {AppRoutingModule} from "./app/app-routing.module";
+// import {AnalyticsService} from "./app/shared/analytics.service";
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [
+    // AnalyticsService
+    importProvidersFrom(AppRoutingModule)
+  ]
+});
